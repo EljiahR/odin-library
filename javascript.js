4865c77c83c0fcf1bd0d0dd07cfe34cebf5d4ff4
Elijah <kcerhajile@gmail.com>
2023-09-17 12:38:00 -0400

    Chagned addToLibrary function to create the book object then push it, instead of just taking a book object to push

--- javascript.js
+++ javascript.js
@@ -10,21 +10,20 @@ Book.prototype.info = function(){
     return this.title
 }
 
-const donQuixote = new Book('Don Quixote', 'Miguel De Cervantes Saavedra', 1072, false);
-const theHobbit = new Book('The Hobbit', 'J. R. R. Tolkien', 300, false);
-const theLWW = new Book('The Lion, the Witch and the Wardrobe', 'C. S. Lewis', 208, true);
 
-const myLibrary = [donQuixote, theHobbit, theLWW];
 
-function addToLibrary(newBook){
+const myLibrary = [];
+
+function addToLibrary(title, author, pages, read){
+    let newBook = new Book(title, author, pages, read)
     myLibrary.push(newBook)
 }
 
-const dummy = new Book('dum', 'dum', 1, false)
-addToLibrary(dummy);
-myLibrary.forEach(book=>{
-    console.log(book.title)
-})
+addToLibrary('Don Quixote', 'Miguel De Cervantes Saavedra', 1072, false);
+addToLibrary('The Hobbit', 'J. R. R. Tolkien', 300, false);
+addToLibrary('The Lion, the Witch and the Wardrobe', 'C. S. Lewis', 208, true);
+addToLibrary('Title', 'Author', 1, false);
+
 
 const cardSection = document.querySelector('.book-cards');
 
