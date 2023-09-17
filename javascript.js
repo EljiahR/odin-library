67f8441ed2c23509106e89ce8735e627f0713af3
Elijah <kcerhajile@gmail.com>
2023-09-17 12:32:54 -0400

    Cards adding in by .js file

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+
 
 function Book(title, author, pages, read){
     this.title = title,
@@ -14,6 +14,8 @@ const donQuixote = new Book('Don Quixote', 'Miguel De Cervantes Saavedra', 1072,
 const theHobbit = new Book('The Hobbit', 'J. R. R. Tolkien', 300, false);
 const theLWW = new Book('The Lion, the Witch and the Wardrobe', 'C. S. Lewis', 208, true);
 
+const myLibrary = [donQuixote, theHobbit, theLWW];
+
 function addToLibrary(newBook){
     myLibrary.push(newBook)
 }
@@ -22,4 +24,29 @@ const dummy = new Book('dum', 'dum', 1, false)
 addToLibrary(dummy);
 myLibrary.forEach(book=>{
     console.log(book.title)
+})
+
+const cardSection = document.querySelector('.book-cards');
+
+myLibrary.forEach(book =>{
+    let card = document.createElement("div");
+    card.classList.add('book-card');
+    let title = document.createElement('h2');
+    title.innerText = book.title;
+    let author = document.createElement('h3');
+    author.innerText = book.author;
+    let pages = document.createElement('p');
+    pages.innerText = `Pages: ${book.pages}`;
+    let readStatus = document.createElement('p');
+    if(book.read){
+        readStatus.innerText = "Read"
+    }else{
+        readStatus.innerText = "Not Read"
+    }
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(readStatus);
+    cardSection.appendChild(card);
+
 })
