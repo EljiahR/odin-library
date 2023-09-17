eff3c8a6fc2778b4c8b724e0be4bb3ff3aa4b595
Elijah <kcerhajile@gmail.com>
2023-09-17 12:01:57 -0400

    Added default data to test in html

--- javascript.js
+++ javascript.js
@@ -10,6 +10,10 @@ Book.prototype.info = function(){
     return this.title
 }
 
+const donQuixote = new Book('Don Quixote', 'Miguel De Cervantes Saavedra', 1072, false);
+const theHobbit = new Book('The Hobbit', 'J. R. R. Tolkien', 300, false);
+const theLWW = new Book('The Lion, the Witch and the Wardrobe', 'C. S. Lewis', 208, true);
+
 function addToLibrary(newBook){
     myLibrary.push(newBook)
 }
